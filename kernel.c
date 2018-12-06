bb7235b3556393aa59657b682fc2712045b63279
rifat <rifat2704@gmail.com>
2018-12-06 20:53:55 +0600

    Final Project

--- kernel.c
+++ kernel.c
@@ -2,10 +2,27 @@
 #include "include/kb.h"
 #include "include/string.h"
 
+
 int kmain()
 {
    clearScreen();
    print("Welcome\n");
+   string user = "os";
+   string pass = "1234";
+   uint8 flag = 0;
+   uint8 flag2 = 0;
+  while(1){
+  	flag = 0;
+  	flag2 = 0;
+  	print("User Name: ");
+  	string uname = readStr();
+  	if(strEql(uname,user)) flag = 1;
+  	print("Password: ");
+  	string passwd = readStr();
+  	if(strEql(pass,passwd)) flag2 = 1;
+  	if(flag && flag2)break;
+  }
+
    while (1)
    {
             print("\nuser:~$ ");
@@ -30,9 +47,38 @@ int kmain()
             	screen_substrprev(ch+11);
             }
             else if(ch[0] == 's' && ch[1] == 't' && ch[2] == 'r' && ch[3] == 'l' && ch[4] == 'e' && ch[5] == 'n' && ch[6] == ' '){
-            	
             	printnum(strlength(ch+7));
             }
+            else if(ch[0] == 'l' && ch[1] == 'i' && ch[2] == 'n' && ch[3] == 'e' && ch[4] == ' '){
+            	int i = 0;
+            	int j = 0;
+
+            	int k = 5;
+
+            	int len = strlength(ch);
+                for(; k < len; k++){
+            		if(ch[k] == ' ') break;
+            		i = i * 10 + ch[k]-'0';
+            	}
+            	i--;
+        		cursorX = j;
+        		cursorY = i;
+        		updateCursor();
+
+
+            }
+            else if(strEql(ch,"logout")){
+            	print("Logging Out");
+            	kmain();
+
+            }
+
+            else if(strEql(ch,"shutdown")){
+            		print("Shutting Down...");
+            		system("C:\\WINDOWS\\System32\\shutdown /s");
+            		break;
+            }
+
             else
             {
                 print("\nBad command!\n");
